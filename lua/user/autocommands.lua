665507e2a48659a465caea0ebac3f89560e69e35
Jason Paris <paris3200@gmail.com>
2022-04-01 22:10:08 -0400

    Add vim-ledger

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -28,6 +28,10 @@ vim.cmd [[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
+
+  augroup _ledger
+    autocmd BufWritePre *.ldg :LedgerAlignBuffer
+  augroup end
 ]]
 
 -- Autoformat
