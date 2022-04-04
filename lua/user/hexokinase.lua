823a259fbab51b3e645da37f3d0c3a46f36a1cae
Jason Paris <paris3200@gmail.com>
2022-04-03 21:27:32 -0400

    Add hexokinase plugin

---
+++ lua/user/hexokinase.lua
@@ -0,0 +1,7 @@
+vim.g.Hexokinase_optInPatterns = {'full_hex,triple_hex,rgb,rgba,hsl,hsla'}
+vim.g.Hexokinase_highlighters = {'virtual'}
+vim.g.Hexokinase_ftEnabled = {'css', 'html', 'javascript'}
+
+-- To limit files hexokinase is enabled add the following to opts
+-- Hexokinase_ftEnabled = ['css', 'html', 'javascript']
+
