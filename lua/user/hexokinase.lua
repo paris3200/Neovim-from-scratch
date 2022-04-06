36e5155ebc37b7bd645bca70b492248d22605c11
Jason Paris <paris3200@gmail.com>
2022-04-06 12:13:48 -0400

    Add processing of sass, scss

--- lua/user/hexokinase.lua
+++ lua/user/hexokinase.lua
@@ -1,6 +1,6 @@
 vim.g.Hexokinase_optInPatterns = {'full_hex,triple_hex,rgb,rgba,hsl,hsla'}
 vim.g.Hexokinase_highlighters = {'virtual'}
-vim.g.Hexokinase_ftEnabled = {'css', 'html', 'javascript'}
+vim.g.Hexokinase_ftEnabled = {'css','scss', 'sass', 'html', 'javascript'}
 
 -- To limit files hexokinase is enabled add the following to opts
 -- Hexokinase_ftEnabled = ['css', 'html', 'javascript']
